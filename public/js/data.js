59c2402697b012e26cf70e27f2c0aae695f0278d
Kendall <kendallrancissegovia@gmail.com>
2018-12-25 02:50:18 +0800

    Added dummy images. Added javascript to fill items based on data. Improved work item styles.

---
+++ public/js/data.js
@@ -0,0 +1,48 @@
+const data = [
+  {
+    title: "Expense Tracker",
+    description:
+      "A single-page application expense tracker with a back-end REST API for storing data. Aaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaa",
+    image: "",
+    links: [
+      {
+        type: "github",
+        path: ""
+      },
+      {
+        type: "site",
+        path: ""
+      }
+    ]
+  },
+  {
+    title: "Where Do I Eat?",
+    description:
+      "A simple Google Maps client that tells the user where to eat based on location and distance.",
+    image: "",
+    links: []
+  },
+  {
+    title: "Where Do I Eat?",
+    description:
+      "A simple Google Maps client that tells the user where to eat based on location and distance.",
+    image: "",
+    links: []
+  },
+  {
+    title: "Where Do I Eat?",
+    description:
+      "A simple Google Maps client that tells the user where to eat based on location and distance.",
+    image: "",
+    links: []
+  },
+  {
+    title: "Where Do I Eat?",
+    description:
+      "A simple Google Maps client that tells the user where to eat based on location and distance.",
+    image: "",
+    links: []
+  }
+];
+
+export default data;
---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+import data from "/js/data.js";
+
+const rowItemLimit = 3;
+
+function displayList() {
+  let sectionContent = document.getElementById("section-content");
+  let list = document.createElement("ul");
+  list.id = "section-content-list";
+  sectionContent.appendChild(list);
+
+  data.forEach(element => {
+    let listItem = document.createElement("li");
+    listItem.classList.add("section-content-item");
+    listItem.innerHTML = `<h1> ${element.title} </h1>`;
+    listItem.innerHTML += `<p> ${element.description} </p>`;
+    list.appendChild(listItem);
+  });
+}
+
+displayList();
